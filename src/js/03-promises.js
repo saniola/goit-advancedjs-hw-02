7b2d9b710d50b2a7585355389bf4c5905b65fb25
Alex Kydanov <a.kidanov@gmail.com>
2024-06-11 21:53:17 +0300

    promises generator

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,45 @@
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const delay = Number(form.elements.delay.value);
+  const step = Number(form.elements.step.value);
+  const amount = Number(form.elements.amount.value);
+
+  for (let i = 0; i < amount; i++) {
+    const currentDelay = delay + i * step;
+    createPromise(i + 1, currentDelay)
+      .then(({ position, delay }) => {
+        iziToast.success({
+          title: 'Success',
+          message: `✅ Fulfilled promise ${position} in ${delay}ms`,
+          position: 'topRight',
+        });
+      })
+      .catch(({ position, delay }) => {
+        iziToast.error({
+          title: 'Error',
+          message: `❌ Rejected promise ${position} in ${delay}ms`,
+          position: 'topRight',
+        });
+      });
+  }
+});
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
